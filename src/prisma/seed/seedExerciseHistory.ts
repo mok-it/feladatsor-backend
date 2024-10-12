67d93e50c961a0d1c6388aab931de1077b636601
KostyalBalint <kostyal.balint@icloud.com>
2024-10-12 22:39:07 +0200

    feat: history seed

--- src/prisma/seed/seedExerciseHistory.ts
+++ src/prisma/seed/seedExerciseHistory.ts
@@ -1,15 +1,23 @@
+import { faker } from '@faker-js/faker';
 import { PrismaClient } from '@prisma/client';
 
 export const seedExerciseHistory = async (prisma: PrismaClient) => {
   const exercises = await prisma.exercise.findMany();
+  const user = await prisma.user.findFirst({});
 
   await Promise.all(
-    exercises.map(async (exercise) => {
-      await prisma.exerciseHistory.create({
-        data: {
-          exercise: { connect: { id: exercise.id } },
-        },
-      });
-    }),
+    exercises.flatMap(async (exercise) =>
+      Array.from({ length: 5 }).flatMap(() =>
+        prisma.exerciseHistory.create({
+          data: {
+            exercise: { connect: { id: exercise.id } },
+            field: faker.database.column(),
+            user: { connect: { id: user.id } },
+            newValue: faker.lorem.slug(10),
+            oldValue: faker.lorem.slug(10),
+          },
+        }),
+      ),
+    ),
   );
 };
