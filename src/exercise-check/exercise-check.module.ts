58709e20b944361f7455e2e4fb43441ac74840cd
KostyalBalint <kostyal.balint@icloud.com>
2024-10-13 13:25:07 +0200

    feat: user for exercise check

--- src/exercise-check/exercise-check.module.ts
+++ src/exercise-check/exercise-check.module.ts
@@ -2,8 +2,10 @@ import { Module } from '@nestjs/common';
 import { ExerciseCheckService } from './exercise-check.service';
 import { ExerciseCheckResolver } from './exercise-check.resolver';
 import { PrismaClient } from '@prisma/client';
+import { UserModule } from '../user/user.module';
 
 @Module({
+  imports: [UserModule],
   providers: [ExerciseCheckResolver, ExerciseCheckService, PrismaClient],
   exports: [ExerciseCheckService],
 })
