490d68fa39d14ba08af8129ea36cda43b1b29a6f
KostyalBalint <kostyal.balint@icloud.com>
2025-06-25 19:47:35 +0200

    feat: dev / prod deploy pipelines

--- src/health/health.controller.ts
+++ src/health/health.controller.ts
@@ -1,6 +1,7 @@
 import { Controller, Get } from '@nestjs/common';
 
 import { HealthCheck, HealthCheckService } from '@nestjs/terminus';
+import { Public } from 'src/auth/decorators/public.decorator';
 import { PrismaHealthIndicator } from '../prisma/PrismaHealthIndicator';
 
 @Controller('health')
@@ -11,12 +12,14 @@ export class HealthController {
   ) {}
 
   @Get('liveness')
+  @Public()
   @HealthCheck()
   live() {
     return this.health.check([]);
   }
 
   @Get('readiness')
+  @Public()
   @HealthCheck()
   ready() {
     return this.health.check([async () => this.prisma.isHealthy('db')]);
